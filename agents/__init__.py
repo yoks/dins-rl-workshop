4c7bb7eaf9d02a5976e7a4396ad6e27eaf749b1d
Ivan <inyokwetrust@gmail.com>
2017-11-26 13:53:35 +0300

    Removed acktr agent

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,2 +1,1 @@
 from .random_agent import *
-from .acktr import *
--- train.py
+++ train.py
@@ -9,11 +9,9 @@ from agents import *
 import datetime
 import ppaquette_gym_super_mario
 
-PROJ_DIR = os.path.dirname(os.path.abspath(__file__))
-
 FLAGS = flags.FLAGS
 flags.DEFINE_string("env", "ppaquette/SuperMarioBros-1-1-v0", "RL environment to train.")
-flags.DEFINE_string("agent", "acktr", "RL algorithm to use.")
+flags.DEFINE_string("agent", "random", "RL algorithm to use.")
 flags.DEFINE_integer("timesteps", 2000000, "Steps to train")
 flags.DEFINE_float("exploration_fraction", 0.5, "Exploration Fraction")
 flags.DEFINE_boolean("prioritized", False, "prioritized_replay")
@@ -32,8 +30,6 @@ def _main(unused_argv):
 
     if FLAGS.agent == "random":
         run_random_agent(FLAGS.env, FLAGS.timesteps)
-    elif FLAGS.agent == 'acktr':
-        train_acktr(FLAGS.env, num_timesteps=int(FLAGS.timesteps), seed=0, num_cpu=FLAGS.num_cpu, learning_rate=FLAGS.lr)
 
 
 if __name__ == '__main__':
